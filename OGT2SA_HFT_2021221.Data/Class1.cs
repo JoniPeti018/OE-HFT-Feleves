22ee61b130cca7852278e1f161bfc548f84e33e8
Péter Jónás <jonipeti2001@jps.hu>
2021-10-08 20:27:04 +0200

    Projektek létrehozása a "solution"-ben

---
+++ OGT2SA_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OGT2SA_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ OGT2SA_HFT_2021221.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OGT2SA_HFT_2021221.Models
+{
+    public class Class1
+    {
+    }
+}
