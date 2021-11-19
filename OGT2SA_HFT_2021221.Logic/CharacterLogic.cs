303621395bdff86345faaf29f98b356376298901
Péter Jónás <jonipeti2001@jps.hu>
2021-11-19 20:09:05 +0100

    CharacterLogic fixed, StudioLogic created

--- OGT2SA_HFT_2021221.Logic/CharacterLogic.cs
+++ OGT2SA_HFT_2021221.Logic/CharacterLogic.cs
@@ -1,4 +1,5 @@
 ﻿using OGT2SA_HFT_2021221.Models;
+using OGT2SA_HFT_2021221.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,36 +8,36 @@ using System.Threading.Tasks;
 
 namespace OGT2SA_HFT_2021221.Logic
 {
-    class CharacterLogic : ICharacterLogic
+    public class CharacterLogic : ICharacterLogic
     {
-        ICharacterLogic characterLogic;
-        public CharacterLogic(ICharacterLogic characterLogic)
+        ICharacterRepository characterRepository;
+        public CharacterLogic(ICharacterRepository characterRepository)
         {
-            this.characterLogic = characterLogic;
+            this.characterRepository = characterRepository;
         }
         public void CreateCharacter(int character_id, int anime_id, int studio_id, string main_character, string main_voice, string support_character, string support_voice)
         {
-            characterLogic.CreateCharacter(character_id, anime_id, studio_id, main_character, main_voice, support_character, support_voice);
+            characterRepository.CreateCharacter(character_id, anime_id, studio_id, main_character, main_voice, support_character, support_voice);
         }
 
         public void DeleteCharacter(int character_id)
         {
-            characterLogic.DeleteCharacter(character_id);
+            characterRepository.DeleteCharacter(character_id);
         }
 
         public IEnumerable<Character> ReadAllCharacter()
         {
-            return characterLogic.ReadAllCharacter();
+            return characterRepository.ReadAllCharacter();
         }
 
         public Character ReadCharacter(int character_id)
         {
-            return characterLogic.ReadCharacter(character_id);
+            return characterRepository.ReadCharacter(character_id);
         }
 
         public void UpdateCharacter(int character_id, int anime_id, int studio_id, string main_character, string main_voice, string support_character, string support_voice)
         {
-            characterLogic.UpdateCharacter(character_id, anime_id, studio_id, main_character, main_voice, support_character, support_voice);
+            characterRepository.UpdateCharacter(character_id, anime_id, studio_id, main_character, main_voice, support_character, support_voice);
         }
     }
 }
---
+++ OGT2SA_HFT_2021221.Logic/StudioLogic.cs
@@ -0,0 +1,43 @@
+﻿using OGT2SA_HFT_2021221.Models;
+using OGT2SA_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OGT2SA_HFT_2021221.Logic
+{
+    public class StudioLogic : IStudioLogic
+    {
+        IStudioRepository studioRepository;
+        public StudioLogic(IStudioRepository studioRepository)
+        {
+            this.studioRepository = studioRepository;
+        }
+        public void CreateStudio(int studio_id, string founded, string studio_name, string founder, string headquarters)
+        {
+            studioRepository.CreateStudio(studio_id, founded, studio_name, founder, headquarters);
+        }
+
+        public void DeleteStudio(int studio_id)
+        {
+            studioRepository.DeleteStudio(studio_id);
+        }
+
+        public IEnumerable<Studio> ReadAllStudio()
+        {
+            return studioRepository.ReadAllStudio();
+        }
+
+        public Studio ReadStudio(int studio_id)
+        {
+            return studioRepository.ReadStudio(studio_id);
+        }
+
+        public void UpdateStudio(int studio_id, string founded, string studio_name, string founder, string headquarters)
+        {
+            studioRepository.UpdateStudio(studio_id, founded, studio_name, founder, headquarters);
+        }
+    }
+}
